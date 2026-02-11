dc8c8dcb60de318ad8da0b9d600b5823f92baa67
nikkidavdev <nichole.davenport.contact@gmail.com>
2026-02-11 13:22:50 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ import nltk
 def get_stopwords():
     nltk.download('stopwords')
 
-st.set_page_config(page_title="Chat with a me about SWITCH", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with a me about SWITCH")
+st.set_page_config(page_title="Chat with me about SWITCH", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Chat with me about SWITCH")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
